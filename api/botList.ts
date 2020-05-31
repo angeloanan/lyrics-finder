e159d2850dc851a8b710d49fa0f92d3730677cc3
Christopher Angelo <angelo@angeloanan.xyz>
2020-06-01 01:54:52 +0700

    feat(botlist): discordextremelist
    Signed-off-by: Christopher Angelo <angelo@angeloanan.xyz>

--- api/botList.ts
+++ api/botList.ts
@@ -2,6 +2,7 @@ import fetch from 'node-fetch'
 require('dotenv').config()
 
 const topggToken = process.env.TOPGG_TOKEN
+const extremeListToken = process.env.EXTREMELIST_TOKEN
 
 async function postTopGG (guildCount: number, botID: string): Promise<void> {
   if (!topggToken) return
@@ -14,14 +15,33 @@ async function postTopGG (guildCount: number, botID: string): Promise<void> {
   fetch(`https://top.gg/api/bots/${botID}/stats`, {
     method: 'POST',
     headers: {
-      Authorization: topggToken
+      Authorization: topggToken,
+      'Content-Type': 'application/json'
     },
     body: JSON.stringify(topggBody)
   })
+}
 
-  console.log(`Guild count has been updated to ${guildCount}`)
+async function postExtremeList (guildCount: number, botID: string): Promise<void> {
+  if (!extremeListToken) return
+
+  const topggBody = {
+    // eslint-disable-next-line @typescript-eslint/camelcase
+    guildCount: guildCount
+  }
+
+  fetch(`https://api.discordextremelist.xyz/v1/bot/${botID}`, {
+    method: 'POST',
+    headers: {
+      Authorization: extremeListToken,
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(topggBody)
+  })
 }
 
 export async function update (guildCount: number, botID: string): Promise<void> {
   postTopGG(guildCount, botID)
+  postExtremeList(guildCount, botID)
+  console.log(`Guild count has been updated to ${guildCount}`)
 }
