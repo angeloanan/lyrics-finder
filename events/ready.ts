5102ed583bed5999fb8d91f5b028e1fcb572ccf7
Christopher Angelo <angelo@angeloanan.xyz>
2020-06-04 20:30:53 +0700

    feat(console-logging): added starting time
    Signed-off-by: Christopher Angelo <angelo@angeloanan.xyz>

--- events/ready.ts
+++ events/ready.ts
@@ -12,7 +12,7 @@ const presenceData: PresenceData = {
 export async function handle (bot: Client): Promise<void> {
   if (bot.user === null) return
   console.log('---')
-  console.log(`Logged in as ${bot.user.username}#${bot.user.discriminator}`)
+  console.log(`Logged in as ${bot.user.username}#${bot.user.discriminator} at ${new Date()}`)
   console.log(`Serving ${bot.users.cache.size - 1} users in ${bot.guilds.cache.size} guilds`)
   console.log(`Admin invite link: ${await bot.generateInvite(['SEND_MESSAGES', 'EMBED_LINKS', 'USE_EXTERNAL_EMOJIS'])}`)
   console.log('---')
