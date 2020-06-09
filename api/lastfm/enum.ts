de0dd072d238d9d141c14f641cac829a658c71ac
Christopher Angelo <angelo@angeloanan.xyz>
2020-06-09 12:28:53 +0700

    feat(lastfm-api): last.fm error codes
    Signed-off-by: Christopher Angelo <angelo@angeloanan.xyz>

---
+++ api/lastfm/enum.ts
@@ -0,0 +1,16 @@
+export const LastFMError = {
+  2: 'Invalid service',
+  3: 'Invalid Method',
+  4: 'Authentication Failed',
+  5: "Invalid format't exist in that format",
+  6: 'Invalid parameters',
+  7: 'Invalid resource specified',
+  8: 'Operation failed',
+  9: 'Invalid session key-authenticate',
+  10: 'Invalid API key.fm',
+  11: 'Service Offline. Try again later.',
+  13: 'Invalid method signature supplied',
+  16: 'There was a temporary error processing your request. Please try again',
+  26: 'Suspended API key, please contact Last.fm',
+  29: 'Rate limit exceeded'
+}
