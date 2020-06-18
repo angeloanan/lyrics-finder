f20ed538ca141b57d69eb3f8b5fed0a4f96628be
Christopher Angelo <angelo@angeloanan.xyz>
2020-06-18 18:30:48 +0700

    feat(botlist): Add support for discord.boats
    Signed-off-by: Christopher Angelo <angelo@angeloanan.xyz>

--- api/botList.ts
+++ api/botList.ts
@@ -7,6 +7,7 @@ const extremeListToken = process.env.EXTREMELIST_TOKEN
 const discordBotListToken = process.env.DBL_TOKEN
 const discordBotsGGToken = process.env.DISCORDBOTSGG_TOKEN
 const glennBotListToken = process.env.GLENNBOTLIST_TOKEN
+const discordBoatsToken = process.env.DISCORDBOATS_TOKEN
 
 async function postTopGG (guildCount: number, botID: string): Promise<void> {
   if (!topggToken) return
@@ -97,6 +98,24 @@ async function postGlennBotList (guildCount: number, botID: string): Promise<voi
   })
 }
 
+async function postDiscordBoats (guildCount: number, botID: string): Promise<void> {
+  if (!discordBoatsToken) return
+
+  const discordBoatsBody = {
+    // eslint-disable-next-line @typescript-eslint/camelcase
+    server_count: guildCount
+  }
+
+  fetch(`https://discord.boats/api/bot/${botID}`, {
+    method: 'POST',
+    headers: {
+      Authorization: discordBoatsToken,
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(discordBoatsBody)
+  })
+}
+
 export async function update (guildCount: number, bot: Client): Promise<void> {
   if (bot.user?.id !== '559265456008200222') return
 
@@ -108,6 +127,7 @@ export async function update (guildCount: number, bot: Client): Promise<void> {
   postDiscordBotList(guildCount, botID, userCount)
   postDiscordBotsGG(guildCount, botID)
   postGlennBotList(guildCount, botID)
+  postDiscordBoats(guildCount, botID)
 
   console.log(`Guild count has been updated to ${guildCount}`)
 }
