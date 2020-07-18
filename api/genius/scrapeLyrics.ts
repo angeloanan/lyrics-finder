f58293b4eb708bbb48781ebb43a72344763fafee
Christopher Angelo <angelo@angeloanan.xyz>
2020-07-18 19:46:10 +0700

    feat: Cloudflare I Am Under Attack Bypass
    Signed-off-by: Christopher Angelo <angelo@angeloanan.xyz>

--- api/genius/scrapeLyrics.ts
+++ api/genius/scrapeLyrics.ts
@@ -3,42 +3,26 @@
 // Things that can go wrong: Lyrics is empty
 
 import fetch from 'node-fetch'
-import cheerio from 'cheerio'
 require('dotenv').config()
 
 // Genius scraping is very unreliable
 // Sometimes, Genius doesn't display the selected tag
 // Limiting this to 5 because 2 tries should be enough to get the lyrics
-// Might wanna to move to a glitch.me API, using cheerio to get the lyrics
+
+if (process.env.BACKEND_URL === undefined) throw new Error('Backend URL not supplied')
+const backend = process.env.BACKEND_URL
+
 export async function scrapeUntilSuccess (url: string): Promise<string> {
-  return fetch(url, {
+  return fetch(backend, {
+    method: 'POST',
     headers: {
-      'Cookie': [
-        `cf_clearance=${process.env.CF_CLEARANCE}`,
-        `__cfduid=${process.env.CFDUID}`
-      ].join('; '),
-      'referer': 'https://genius.com/search/embed',
-      'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-      'Accept-Encoding': 'gzip, deflate, br',
-      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36 Edg/83.0.478.64'
-    }
-  })
-    .then(res => res.text())
-    .then(async res => {
-      // Using Cheerio to parse HTML, accessible using JQuery syntax
-      // Loads the page HTML
-      const $ = cheerio.load(res)
-
-      // Selects <div class="lyrics"> and returns the text
-      let lyrics = $('div[class=lyrics]').text()
-
-      // Trims whitespaces
-      lyrics.trim()
-
-      // Recursive
-      // eslint-disable-next-line @typescript-eslint/no-use-before-define
-      if (lyrics === '') { lyrics = await scrapeLyricsFromURL(url) }
-      return lyrics
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ url: url })
+  }).then(res => res.text())
+    .then(res => {
+      // if (res.length <= 5) scrapeUntilSuccess(url)
+      return res
     })
 }
 
