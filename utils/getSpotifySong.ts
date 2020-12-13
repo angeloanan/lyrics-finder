3634fcffba9eaf197d6e7d48f3492d877c30f339
Angelo <angelo@angeloanan.xyz>
2020-12-13 07:38:43 +0700

    feat(getSpotifySong): asynchronous

--- utils/getSpotifySong.ts
+++ utils/getSpotifySong.ts
@@ -1,30 +1,45 @@
 import type { Presence } from 'discord.js'
+import { newError } from '.'
 
 /**
  * Gets an user's currently playing Spotify song
  * @param presence User's Presence
  * @returns {string} Song Title and Artist: `<Song Name> - <Song Artist>`
  */
-export function getSpotifySong (presence: Presence): Promise<string> {
-  return new Promise((resolve, reject) => {
+export async function getSpotifySong (presence: Presence): Promise<string> {
+  try {
     const presences = presence.activities
 
-    for (const presence of presences) {
-      if (presence.name === 'Spotify' && presence.applicationID === null) {
-        // Spotify - Presence#state = Artists, seperated by `;`
-        // Spotify - Presence#details = Song Title
+    const spotifyPresence = await Promise.any([
+      ...presences.map(async (presence) => {
+        if (presence.name === 'Spotify' && presence.applicationID === null) {
+          // Spotify - Presence#state = Artists, seperated by `;`
+          // Spotify - Presence#details = Song Title
 
-        const songArtist = presence.state || ''
-        const songPrimaryArtist = songArtist.split(';')[0] || songArtist // Only takes the first artist
+          const songArtist = presence.state ?? ''
+          const songPrimaryArtist = songArtist.split(';')[0] ?? songArtist // Only takes the first artist
 
-        const songTitle = presence.details || ''
-        const songTitleCleanBrackets = songTitle.split('(')[0] // Cleans anything brackets
-        const songTitleCleanDash = songTitleCleanBrackets.split('-')[0] // Cleans anything brackets
+          const songTitle = presence.details ?? ''
+          const songTitleCleanBrackets = songTitle.split('(')[0] // Cleans anything brackets
+          const songTitleCleanDash = songTitleCleanBrackets.split('-')[0] // Cleans anything brackets
 
-        const cleanSongTitle = songTitleCleanDash
-        return resolve(`${cleanSongTitle} ${songPrimaryArtist}`)
-      }
+          const cleanSongTitle = songTitleCleanDash
+          return `${cleanSongTitle} ${songPrimaryArtist}`
+        }
+
+        throw new Error('Not found')
+      })
+    ])
+
+    return spotifyPresence
+  } catch (e) {
+    const error = e as Error
+
+    switch (error.name) {
+      case 'AggregateError':
+        throw newError('Not found', 'Spotify song not found')
+      default:
+        throw e
     }
-    reject(new Error('Spotify Song Not Found'))
-  })
+  }
 }
