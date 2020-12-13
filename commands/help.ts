2b3df24c6255847931df520249550fc8dbe4b613
Angelo <angelo@angeloanan.xyz>
2020-12-13 08:16:58 +0700

    refactor(cmd-help): complete refactor
    * Move help fields to a constants file
    * Handle error

--- commands/help.ts
+++ commands/help.ts
@@ -1,61 +1,19 @@
-import { Client, Message, EmbedField, MessageEmbed } from 'discord.js'
+import { Client, Message } from 'discord.js'
 
-export function wrapInCodeblocks (str: string[]): string {
-  let out = '```\n'
-  for (const s of str) {
-    out += s + '\n'
-  }
-  out += '```'
-
-  return out
-}
-
-const helpFields: EmbedField[] = [
-  {
-    name: '🎶 • Lyrics (**3**)',
-    value: wrapInCodeblocks([
-      'search',
-      'nowplaying',
-      'autosearch'
-    ]),
-    inline: true
-  },
-  {
-    name: '💬 • Bot Info (**5**)',
-    value: wrapInCodeblocks([
-      'help',
-      'ping',
-      'invite',
-      'stats',
-      'privacypolicy'
-    ]),
-    inline: true
-  },
-  {
-    name: '⛏ • Admin Commands (**1**)',
-    value: wrapInCodeblocks([
-      'stopautosearch'
-    ]),
-    inline: true
-  }
-]
+import { HelpEmbed } from '../constants/embeds'
+import logger from '../utils/logger'
 
 export async function exec (bot: Client, message: Message): Promise<void> {
-  const query = message.content.split(' ')[1] ?? ''
-  const embed = new MessageEmbed()
-    .setColor('2F3136')
+  try {
+    const helpMessage = HelpEmbed(bot)
 
-  switch (query) {
-    default:
-      embed.setAuthor('Lyrics Finder Help | Commands', bot.user?.displayAvatarURL())
-      embed.setDescription('Here is the bot\'s available commands.\nJust add the prefix `~!` before one of the commands')
-      embed.setFooter('Something doesn\'t work or need help? Join the support server!')
-      embed.addFields(helpFields)
-      break
+    await message.channel.send(helpMessage)
+  } catch (e) {
+    try {
+      const error = e as Error
+      await message.channel.send(`I am not able to send Embeds!\nPlease recheck the bot's permission\n\`${error.message}\``)
+    } catch (ee) {
+      logger.error({ e, ee }, 'Unable to send help message')
+    }
   }
-
-  message.channel.send(embed)
-    .catch(err => {
-      message.channel.send(`I am not able to send Embeds!\nPlease recheck the bot's permission\n\`${err}\``)
-    })
 }
