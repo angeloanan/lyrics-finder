98e89baedd021106a864996833c0657c75e0cf2b
Christopher Angelo <angelo@angeloanan.xyz>
2020-12-30 01:15:28 +0700

    feat(guildCountUpdateEvent): future-proof event handlers to use async promise

--- events/guildCountUpdate.ts
+++ events/guildCountUpdate.ts
@@ -1,11 +1,15 @@
+import { DiscordClient } from '../types/DiscordClient'
 import type { Guild } from 'discord.js'
 import { update } from '../api/botList'
-import { DiscordClient } from '../types/DiscordClient'
 
 // List your event handlers here.
 async function main (bot: DiscordClient, _guild: Guild): Promise<void> {
-  if (!bot.user) return
-  update(bot.guilds.cache.size, bot)
+  if (bot.user == null) return
+
+  // Asynchronously update events
+  await Promise.allSettled([
+    update(bot.guilds.cache.size, bot)
+  ])
 }
 
 exports.handle = main
