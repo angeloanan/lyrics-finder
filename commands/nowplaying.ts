51b183a04848136b3c2008d907375cf4ed4d0b9b
Christopher Angelo <angelo@angeloanan.xyz>
2021-03-20 05:26:57 +0700

    fix(nowplaying): force fetch a user's presence

--- commands/nowplaying.ts
+++ commands/nowplaying.ts
@@ -6,7 +6,7 @@ import { getSpotifySong } from '../utils/getSpotifySong'
 
 export async function nowPlaying (_bot: Client, message: Message): Promise<void> {
   const responseMessage = message.channel.send(BarebonesLyricsEmbed())
-  const messageAuthor = await message.author.fetch()
+  const messageAuthor = await message.author.fetch(true)
 
   responseMessage.catch((err) => {
     message.channel.send(`I am not able to send embeds here!\nPlease recheck the permission of the bot!\`${err}\``)
